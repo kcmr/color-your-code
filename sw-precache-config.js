00c097e098f1c63498bc81bcff36125441ecf248
Kus Cámara <kcmr@users.noreply.github.com>
2018-03-26 18:32:26 +0200

    initial commit

---
+++ sw-precache-config.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+
+module.exports = {
+  staticFileGlobs: [
+    'bower_components/webcomponentsjs/webcomponents-loader.js',
+    'manifest.json',
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'webcomponentsjs-polyfills-cache',
+        },
+      },
+    },
+  ],
+};
