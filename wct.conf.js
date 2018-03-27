49a3aa250f27e316611ff2fda25e9adcd256ffaf
Kus Cámara <kcmr@users.noreply.github.com>
2018-03-27 22:59:57 +0200

    chore(test): add coverage

---
+++ wct.conf.js
@@ -0,0 +1,32 @@
+module.exports = {
+  suites: [
+    'test'
+  ],
+  expanded: true,
+  plugins: {
+    local: {
+      browsers: ['chrome']
+    },
+    istanbulcoverage: {
+      dir: './coverage',
+      reporters: [
+        'text',
+        'text-summary',
+        'lcov',
+        'json'
+      ],
+      include: [
+        'dist/**/*.html'
+      ],
+      exclude: [],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
+    }
+  }
+};
