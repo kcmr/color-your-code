8ffaf5b96852ca852f3ad8e04c76756cd01fcb31
Kus Cámara <kcmr@users.noreply.github.com>
2018-07-30 20:20:32 +0200

    feat(sidebar): add highlighting

--- src/cyc-editor-sidebar/cyc-editor-sidebar.js
+++ src/cyc-editor-sidebar/cyc-editor-sidebar.js
@@ -1,12 +1,15 @@
 {
   const {Element} = Polymer;
+  const {HighlightMixin} = ColorYourCode;
 
   /**
    * `<cyc-editor-sidebar>` displays the editor sidebar.
    * @polymer
    * @customElement
+   * @memberof ColorYourCode
+   * @appliesMixin ColorYourCode.HighlightMixin
    */
-  class CycEditorSidebar extends Element {
+  class CycEditorSidebar extends HighlightMixin(Element) {
     static get is() {
       return 'cyc-editor-sidebar';
     }
