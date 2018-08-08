ddeaf9e2ba8cb34dd96651d4c21d9b4f722fd7a7
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-08 18:25:02 +0200

    feat(editor-sidebar): allow to select a file from the list

--- src/cyc-editor-sidebar/cyc-editor-sidebar.js
+++ src/cyc-editor-sidebar/cyc-editor-sidebar.js
@@ -1,5 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {highlightMixin} from '../cyc-mixins/cyc-highlight-mixin.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
 
 /**
  * `<cyc-editor-sidebar>` displays the editor sidebar.
@@ -15,15 +16,65 @@ class CycEditorSidebar extends highlightMixin(PolymerElement) {
     <link rel="stylesheet" href="cyc-editor-sidebar.css" inline>
 
     <div class="heading panel-heading" data-prop="sideBarTitle.foreground" on-mouseenter="_onSectionMouseenter">Explorador</div>
-    <div class="sidebar-panel" data-target-prop="editor.foreground" on-mouseenter="_onSectionMouseenter">
-      <div class="heading sidebar-heading" data-prop="editor.foreground">Editores abiertos</div>
+    <div class="sidebar-panel">
+      <div class="heading sidebar-heading" data-prop="editor.foreground" on-mouseenter="_onSectionMouseenter">Editores abiertos</div>
       <ul class="open-files">
-        <li class="modified selected">some-file.html</li>
-        <li data-prop="editor.foreground">some-file.js</li>
-        <li data-prop="editor.foreground">some-file.scss</li>
+        <template is="dom-repeat" items="[[_openFiles]]">
+          <li class$="[[_computeClass(_selectedFileType, item)]]" on-click="_selectFile">[[item.name]]</li>
+        </template>
       </ul>
     </div>
     `;
+  }
+
+  static get properties() {
+    return {
+      _selectedFileType: {
+        type: String,
+        value: 'html',
+      },
+
+      _openFiles: {
+        type: Array,
+        value: () => [{
+          name: 'some-file.html',
+          type: 'html',
+          modified: true,
+        }, {
+          name: 'some-file.js',
+          type: 'js',
+          modified: false,
+        }, {
+          name: 'some-file.css',
+          type: 'css',
+          modified: false,
+        }],
+      },
+    };
+  }
+
+  _computeClass(selectedFileType, item) {
+    const {type, modified} = item;
+
+    const modifiedClass = modified ? 'modified' : '';
+    const selectedClass = (selectedFileType === type) ? 'selected' : '';
+    const className = `${modifiedClass} ${selectedClass}`;
+
+    return className;
+  }
+
+  _selectFile(event) {
+    event.stopPropagation();
+
+    const type = event.model.item.type;
+    this._selectedFileType = type;
+
+    /**
+     * Fired after selecting a file.
+     * @event selected-file
+     * @param {String} type file extension (html | js | css).
+     */
+    this._emit('selected-file', type);
   }
 }
 
